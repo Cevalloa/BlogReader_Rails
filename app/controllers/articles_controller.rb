d7912e4969fbfab0a165ea6a4a89a73952b51154
Alex Cevallos <cevalloa@gmail.com>
2015-05-20 14:00:00 +0100

    Showing new article view: title & post after new POST

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,8 +3,24 @@ class ArticlesController < ApplicationController
 
 	end
 
-	def create
-		render plain: params[:article].inspect
+	def show
+
+		@article = Article.find(params[:id])
+
 	end
+	def create
+		#render plain: params[:article].inspect
+
+		#@article = Article.new(params[:article]).permit(:title, :text))
+		@article = Article.new(article_params)
+
+		@article.save
+		redirect_to @article
+	end
+
+	private
+		def article_params
+			params.require(:article).permit(:title, :text)
+		end
 
 end
